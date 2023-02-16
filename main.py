0fd5e940a778362588bb264336c5bf55f374465c
guava <guavareigen2@gmail.com>
2023-02-16 18:21:28 +0530

    email functionality added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import *
 
 API_key = "1ecd2b254398465fb92ac340347869f1"
 
@@ -8,6 +9,11 @@ url="https://newsapi.org/v2/everything?q=apple&from=2023-02-15& \
 
 req = requests.get(url)
 content = req.json()
+body = " "
 for article in content["articles"]:
-    print(article["title"])
+    if article["title"] is not None:
+        body = body + (article["title"]) + "\n" + (article["description"]) + "\n" + "\n "
 
+body = body.encode("utf-8")
+
+send_email(message=body)
